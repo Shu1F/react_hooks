c0073170859ce1a4833448a0fb583a291fef9295
Shuichi Fujiike <fs2031@icloud.com>
2025-12-28 15:23:34 +0900

    lesson-1-1 finished

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Lesson1_1 = () => {
   const [age, setAge] = useState<number>(0);
+  const [name, setName] = useState("ShinCode");
 
   console.log("rendering!");
 
@@ -9,11 +10,19 @@ const Lesson1_1 = () => {
     setAge((state) => state + 1);
     setAge((state) => state + 1);
     setAge((state) => state + 1);
+
+    console.log(name);
   };
 
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setName(e.target.value)
+        }
+      />
       <button
         onClick={handleAddAge}
         className="border p-2 rounded-md bg-red-100"
