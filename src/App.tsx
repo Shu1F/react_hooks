1a2a0e125c33f645a48d93dcc1f38f4fe54af2aa
Shuichi Fujiike <fs2031@icloud.com>
2025-12-29 12:19:35 +0900

    lesson2_2finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Lesson2_2 from "./lessons/Lesson2/Lesson2_2/Lesson2_2";
+import Lesson2_3 from "./lessons/Lesson2/Lesson2_3/Lesson2_3";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       {/* <Lesson1_1 /> */}
       {/* <Lesson1_2 /> */}
       {/* <Lesson2_1 /> */}
-      <Lesson2_2 />
+      {/* <Lesson2_2 /> */}
+      <Lesson2_3 />
       {/* <Lesson3_1 /> */}
       {/* ... */}
     </div>
