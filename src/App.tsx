865cec4a54a2f1b37484ee8e7978a58e8c98dce7
Shuichi Fujiike <fs2031@icloud.com>
2025-12-29 13:41:53 +0900

    lesson3-3 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 // import Lesson2_3 from "./lessons/Lesson2/Lesson2_3/Lesson2_3";
-import Lesson3_2 from "./lessons/Lesson3/Lesson3_2";
+import Lesson3_3 from "./lessons/Lesson3/Lesson3_3";
 
 function App() {
   return (
@@ -11,7 +11,8 @@ function App() {
       {/* <Lesson2_2 /> */}
       {/* <Lesson2_3 /> */}
       {/* <Lesson3_1 /> */}
-      <Lesson3_2 />
+      {/* <Lesson3_2 /> */}
+      <Lesson3_3 />
       {/* ... */}
     </div>
   );
--- src/lessons/Lesson3/Lesson3_3.tsx
+++ src/lessons/Lesson3/Lesson3_3.tsx
@@ -1,9 +1,17 @@
+import { useRef } from "react";
+
 const Lesson3_3 = () => {
-  const handleClick = () => {};
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log("rendering!!");
+
+  const handleClick = () => {
+    alert(inputRef.current?.value);
+  };
 
   return (
     <div>
-      <input type="text" className="border-b" />
+      <input type="text" className="border-b" ref={inputRef} />
       <button onClick={handleClick}>input入力値を見る</button>
     </div>
   );
